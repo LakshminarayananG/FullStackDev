21b797a540d9f07846a56f143226eb39b6ca8bf8
Lakshminarayanan <lakshnarayanan7@gmail.com>
2021-07-31 11:21:12 +0530

    Spring Sample Project Practice

---
+++ spring-demo/src/main/java/com/practice/demo1/Demo.java
@@ -0,0 +1,13 @@
+package com.practice.demo1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Demo {
+
+	
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("demo1.xml");
+
+	}
+}
